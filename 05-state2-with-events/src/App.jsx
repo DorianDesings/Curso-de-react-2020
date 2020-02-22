0e998c9693e2a0eabc8b2eceb058e8559530922f
Dorian Desings <dorianoriginaldesings@gmail.com>
2020-02-22 14:06:53 +0100

    Clase 05 terminada

---
+++ 05-state2-with-events/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    state = { number: this.props.number }
+
+    handleIncrement = () => {
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+
+    handleDecrement = () => {
+        this.setState({
+            number: this.state.number - 1
+        })
+    }
+
+    handleReset = () => {
+        this.setState({
+            number: 0
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Number: {this.state.number}</h1>
+                <button onClick={this.handleIncrement}>Increment</button>
+                <button onClick={this.handleDecrement}>Decrement</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </>
+        );
+    }
+}
+
+App.defaultProps = {
+    number: 0
+}
+
+
+export default App;
---
+++ 05-state2-with-events/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+
+ReactDOM.render(<App number={10} />, document.getElementById('root'));
