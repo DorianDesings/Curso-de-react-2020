bea59ff6843818b781d0038b5cb3d7af40a48336
Dorian Desings <dorianoriginaldesings@gmail.com>
2020-02-29 02:41:22 +0100

    Binding en clases

--- 07-events/src/App.jsx
+++ 07-events/src/App.jsx
@@ -4,11 +4,22 @@ class App extends Component {
 
     constructor(props) {
         super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        console.log(this)
     }
 
     render() {
         return (
-            <h1>Events</h1>
+            <>
+                <h1>Events</h1>
+                {/* <button onClick={this.handleClick}>Click me!</button> */}
+                {/* <button onClick={() => this.handleClick()}>Click me!</button> */}
+                {/* <button onClick={this.handleClick.bind(this)}>Click me!</button> */}
+                <button onClick={this.handleClick}>Click me!</button>
+            </>
         )
     }
 }
