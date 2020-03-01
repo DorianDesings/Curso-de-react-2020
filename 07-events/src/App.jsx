f35d3baab6c17bba71506b9f9fcf831bb5d3bb29
Dorian Desings <dorianoriginaldesings@gmail.com>
2020-03-01 06:01:04 +0100

    Clase 7 terminada

--- 07-events/src/App.jsx
+++ 07-events/src/App.jsx
@@ -2,22 +2,15 @@ import React, { Component } from 'react'
 
 class App extends Component {
 
-    constructor(props) {
-        super(props)
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick() {
-        console.log(this)
+    handleClick(e) {
+        console.log(e)
+        console.log(e.nativeEvent)
     }
 
     render() {
         return (
             <>
                 <h1>Events</h1>
-                {/* <button onClick={this.handleClick}>Click me!</button> */}
-                {/* <button onClick={() => this.handleClick()}>Click me!</button> */}
-                {/* <button onClick={this.handleClick.bind(this)}>Click me!</button> */}
                 <button onClick={this.handleClick}>Click me!</button>
             </>
         )
