b27176f44c47ddaf858ae581f23b85a6a50edba7
Dorian Desings <dorianoriginaldesings@gmail.com>
2020-03-16 07:53:58 +0100

    Clase 11 creada

---
+++ 11-hooks/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Button from './components/Button/Button';
+
+class App extends Component {
+
+    render() {
+        return <Button />
+    }
+}
+
+export default App;
