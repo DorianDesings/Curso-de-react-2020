9583c6b8faa712584180bafab9d1764215af4812
Adrian <adrian.heras@evoluciona.com>
2020-04-10 00:31:07 +0200

    clase 13 creada

---
+++ 13-to-do-list/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <h1>To do list</h1>
+
+        )
+    }
+}
+
+export default App;
