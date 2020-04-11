1773cda42047cf711ce79c6dac524a10c22d1412
Adrian <adrian.heras@evoluciona.com>
2020-04-11 17:46:30 +0200

    Clase 13 terminada

--- 13-to-do-list/src/App.jsx
+++ 13-to-do-list/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { createGlobalStyle } from 'styled-components'
 
 import { generate as id } from 'shortid'
@@ -16,98 +16,79 @@ const GlobalSyle = createGlobalStyle`
     }
 `
 
-class App extends Component {
+const App = () => {
 
-    state = {
-        colorSelected: allColors.colors[0],
-        tasks: [
-            {
-                title: 'Aprender react',
-                color: allColors.colors[0],
-                done: false
-            }
-        ]
-    }
+    const [colorSelected, setColorSelected] = useState(allColors.colors[0])
+    const [tasks, setTasks] = useState([])
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         if (e.target.title.value.trim() !== '') {
-            this.createNewTask(e.target.title.value)
+            createNewTask(e.target.title.value)
             e.target.title.value = ''
         }
     }
 
-    createNewTask = (title) => {
+    const createNewTask = (title) => {
         const newTask = {
             id: id(),
             title,
-            color: this.state.colorSelected,
+            color: colorSelected,
             done: false
         }
 
-        const allTasks = [...this.state.tasks, newTask]
+        const allTasks = [...tasks, newTask]
 
-        this.setState({ tasks: allTasks })
+        setTasks(allTasks)
     }
 
-    getTask = (id) => {
-        const task = this.state.tasks.find(task => task.id === id)
-        return task
-    }
-
-    handleCompleteTask = (id) => {
-        const currentTasks = this.state.tasks
-        const task = this.getTask(id)
+    const handleCompleteTask = (id) => {
+        const currentTasks = [...tasks]
+        const task = currentTasks.find(task => task.id === id)
         const index = currentTasks.indexOf(task)
 
         currentTasks[index].done = !currentTasks[index].done
 
-        this.setState({ tasks: currentTasks })
+        setTasks(currentTasks)
     }
 
-    handleDeleteTask = (id) => {
-        let currentTasks = this.state.tasks
+    const handleDeleteTask = (id) => {
+        let currentTasks = [...tasks]
         currentTasks = currentTasks.filter(task => task.id !== id)
 
-        this.setState({ tasks: currentTasks })
+        setTasks(currentTasks)
     }
 
-    handleChangeColor = (color) => {
-        this.setState({ colorSelected: color })
+    const handleChangeColor = (color) => {
+        setColorSelected(color)
     }
 
-    render() {
-        const { colorSelected, tasks } = this.state
-        return (
-            <>
-                <GlobalSyle />
-                <h1>To do list</h1>
-                <FormTask
-                    handleChangeColor={this.handleChangeColor}
-                    handleSubmit={this.handleSubmit}
-                    colorSelected={colorSelected}
-                />
+    return (
+        <>
+            <GlobalSyle />
+            <h1>To do list</h1>
+            <FormTask
+                handleChangeColor={handleChangeColor}
+                handleSubmit={handleSubmit}
+                colorSelected={colorSelected}
+            />
 
-                {this.state.tasks.length === 0 && <p>Not tasks yet</p>}
+            {tasks.length === 0 && <p>Not tasks yet</p>}
+            {
+                tasks.map(task => (
+                    <Task
+                        key={id()}
+                        done={task.done}
+                        title={task.title}
+                        color={task.color}
+                        handleCompleteTask={() => handleCompleteTask(task.id)}
+                        handleDeleteTask={() => handleDeleteTask(task.id)}
+                    />
+                ))
+            }
+        </>
 
-                <div>
-                    {
-                        tasks.map(task => (
-                            <Task
-                                key={id()}
-                                done={task.done}
-                                title={task.title}
-                                color={task.color}
-                                handleCompleteTask={() => this.handleCompleteTask(task.id)}
-                                handleDeleteTask={() => this.handleDeleteTask(task.id)}
-                            />
-                        ))
-                    }
-                </div>
-            </>
-
-        )
-    }
+    )
 }
 
 export default App;
